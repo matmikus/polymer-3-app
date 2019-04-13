ae0f1fbf376ceb462fa0ce44f8e8ec1b4c124540
matmikus <mateusz.mikolajczak@yahoo.com>
2019-04-13 14:18:55 +0200

    path fixed

--- my-element.js
+++ my-element.js
@@ -1,4 +1,4 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js'
+import { PolymerElement, html } from '/node_modules/@polymer/polymer/polymer-element.js'
 
 class myElement extends PolymerElement {
   static get template () {
